47809bdabf9834f492484305a90f1d508cc2ae42
macsDevelop008 <47404938+macsDevelop008@users.noreply.github.com>
2025-02-09 08:38:16 -0500

    🔧➕ Se agregan depentencias y configuraciones para Firebase en Android e iOS.

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:lego_express/presentation/presentation.dart';
 
 void main() async {
-  // Cargar las variables de entorno.
+  // Inicializar Firebase
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  // Inicializar las variables de entorno.
   await dotenv.load(fileName: ".env");
   // Ejecutar la aplicación.
   runApp(const MyApp());
